5cc9f094e249cef0b8c562ab91c42a1ddb89d404
sergio_remoto <sresquin@outlook.com>
2024-11-09 08:19:21 -0300

    Mostrando la información de cada guitarra con props

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,11 @@ function App() {
 
         <div className="row mt-5">
 
-          {data.map(() => (
-            <Guitar />
+          {data.map((guitar) => (
+            <Guitar 
+              key={guitar.id}
+              guitar={guitar}
+            />
           ))}
             
         </div>
